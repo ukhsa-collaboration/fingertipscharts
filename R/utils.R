9bf9b4f5d07b32f42602782a2dd71d21dd33b182
Sebastian Fox <sebastian.fox@phe.gov.uk>
2019-09-09 16:09:36 +0100

    minor changes

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,5 @@
 #' Check if the given ONS json is available
 #' @inheritParams map
-#' @examples
-#' ons_api <- "https://opendata.arcgis.com/datasets/687f346f5023410ba86615655ff33ca9_4.geojson"
-#' ensure_ons_api_available(ons_api)
 #' @return \code{TRUE} if the API is available, otherwise \code{stop()} is called.
 ensure_ons_api_available <- function(ons_api) {
         code <- FALSE
