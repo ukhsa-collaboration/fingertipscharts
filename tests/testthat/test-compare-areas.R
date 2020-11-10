3be821a6e6ca0ee4c45df4b1271391580e951be7
Sebastian Fox <sebastian.fox@phe.gov.uk>
2020-11-10 09:28:11 +0000

    test-map and test-compare-areas converted to testhtat edition 3.0.0

--- tests/testthat/test-compare-areas.R
+++ tests/testthat/test-compare-areas.R
@@ -1,90 +1,100 @@
-context("compare_areas")
-library(fingertipscharts)
-df <- create_test_data()
-
-parent <- "PAC11"
-top_names <- c("C001", parent)
-ordered_levels <- c("Better",
-                    "Similar",
-                    "Worse",
-                    "Not compared")
-df_ca <- df %>%
-        filter(IndicatorName == "Indicator 3",
-               (AreaCode %in% top_names |
-                        ParentAreaCode == parent))
-desc_p <- compare_areas(df_ca, AreaCode, Value,
-                   fill = Significance,
-                   lowerci = LCI,
-                   upperci = UCI,
-                   order = "desc",
-                   top_areas = top_names,
-                   title = "Compare the local areas")
-
-desc_no_top_p <- compare_areas(df_ca, AreaCode, Value,
-                   fill = Significance,
-                   lowerci = LCI,
-                   upperci = UCI,
-                   order = "desc",
-                   title = "Compare the local areas")
-
-asc_p <- compare_areas(df_ca, AreaCode, Value,
-                   fill = Significance,
-                   lowerci = LCI,
-                   upperci = UCI,
-                   order = "asc",
-                   top_areas = top_names,
-                   title = "Compare the local areas")
-
-asc_no_top_p <- compare_areas(df_ca, AreaCode, Value,
-                             fill = Significance,
-                             lowerci = LCI,
-                             upperci = UCI,
-                             order = "asc",
-                             title = "Compare the local areas")
-
-desc_p_no_fill <- compare_areas(df_ca, AreaCode, Value,
-                        lowerci = LCI,
-                        upperci = UCI,
-                        order = "desc",
-                        top_areas = top_names,
-                        title = "Compare the local areas")
-
-desc_p_no_fill_disp_vals <- compare_areas(df_ca, AreaCode, Value,
-                                          lowerci = LCI,
-                                          upperci = UCI,
-                                          order = "desc",
-                                          top_areas = top_names,
-                                          title = "Compare the local areas",
-                                          display.values = TRUE,
-                                          dps = 2)
 
 # Visual tests ------------------------------------------------------------
 
 test_that("desc compare areas draws correctly", {
+
+        parent <- "PAC11"
+        top_names <- c("C001", parent)
+        df_ca <- create_test_data() %>%
+                filter(IndicatorName == "Indicator 3",
+                       (AreaCode %in% top_names |
+                                ParentAreaCode == parent))
+        desc_p <- compare_areas(df_ca, AreaCode, Value,
+                                fill = Significance,
+                                lowerci = LCI,
+                                upperci = UCI,
+                                order = "desc",
+                                top_areas = top_names,
+                                title = "Compare the local areas")
         vdiffr::expect_doppelganger("desc compare areas",
                                     desc_p
         )
 })
 
 test_that("no top desc compare areas draws correctly", {
+        parent <- "PAC11"
+        top_names <- c("C001", parent)
+        df_ca <- create_test_data() %>%
+                filter(IndicatorName == "Indicator 3",
+                       (AreaCode %in% top_names |
+                                ParentAreaCode == parent))
+
+        desc_no_top_p <- compare_areas(df_ca, AreaCode, Value,
+                                       fill = Significance,
+                                       lowerci = LCI,
+                                       upperci = UCI,
+                                       order = "desc",
+                                       title = "Compare the local areas")
+
         vdiffr::expect_doppelganger("no top areas desc compare areas",
                                     desc_no_top_p
         )
 })
 
 test_that("asc compare areas draws correctly", {
+        parent <- "PAC11"
+        top_names <- c("C001", parent)
+        df_ca <- create_test_data() %>%
+                filter(IndicatorName == "Indicator 3",
+                       (AreaCode %in% top_names |
+                                ParentAreaCode == parent))
+
+        asc_p <- compare_areas(df_ca, AreaCode, Value,
+                               fill = Significance,
+                               lowerci = LCI,
+                               upperci = UCI,
+                               order = "asc",
+                               top_areas = top_names,
+                               title = "Compare the local areas")
         vdiffr::expect_doppelganger("asc compare areas",
                                     asc_p
         )
 })
 
 test_that("no top asc compare areas draws correctly", {
+        parent <- "PAC11"
+        top_names <- c("C001", parent)
+        df_ca <- create_test_data() %>%
+                filter(IndicatorName == "Indicator 3",
+                       (AreaCode %in% top_names |
+                                ParentAreaCode == parent))
+
+        asc_no_top_p <- compare_areas(df_ca, AreaCode, Value,
+                                      fill = Significance,
+                                      lowerci = LCI,
+                                      upperci = UCI,
+                                      order = "asc",
+                                      title = "Compare the local areas")
+
         vdiffr::expect_doppelganger("no top areas asc compare areas",
                                     asc_no_top_p
         )
 })
 
 test_that("desc compare areas no fill draws correctly", {
+        parent <- "PAC11"
+        top_names <- c("C001", parent)
+        df_ca <- create_test_data() %>%
+                filter(IndicatorName == "Indicator 3",
+                       (AreaCode %in% top_names |
+                                ParentAreaCode == parent))
+
+        desc_p_no_fill <- compare_areas(df_ca, AreaCode, Value,
+                                        lowerci = LCI,
+                                        upperci = UCI,
+                                        order = "desc",
+                                        top_areas = top_names,
+                                        title = "Compare the local areas")
         vdiffr::expect_doppelganger("desc no fill compare areas",
                                     desc_p_no_fill
         )
@@ -92,6 +102,21 @@ test_that("desc compare areas no fill draws correctly", {
 
 
 test_that("desc compare areas no fill draws correctly while displaying values to 2 dps", {
+        parent <- "PAC11"
+        top_names <- c("C001", parent)
+        df_ca <- create_test_data() %>%
+                filter(IndicatorName == "Indicator 3",
+                       (AreaCode %in% top_names |
+                                ParentAreaCode == parent))
+
+        desc_p_no_fill_disp_vals <- compare_areas(df_ca, AreaCode, Value,
+                                                  lowerci = LCI,
+                                                  upperci = UCI,
+                                                  order = "desc",
+                                                  top_areas = top_names,
+                                                  title = "Compare the local areas",
+                                                  display.values = TRUE,
+                                                  dps = 2)
         vdiffr::expect_doppelganger("desc no fill compare areas displaying values",
                                     desc_p_no_fill_disp_vals
         )
--- tests/testthat/test-map.R
+++ tests/testthat/test-map.R
@@ -1,36 +1,41 @@
-context("map")
 
-library(dplyr)
 
-df <- mapdata
-ons_api <- "https://opendata.arcgis.com/datasets/687f346f5023410ba86615655ff33ca9_4.geojson"
-
-p <- map(df,
-         ons_api = ons_api,
-         area_code = AreaCode,
-         fill = Significance,
-         title = "Life expectancy at birth",
-         subtitle = "Males in Upper Tier Local Authorities England",
-         copyright_year = 2019)
-p1 <- map(df,
-          ons_api = ons_api,
-          area_code = AreaCode,
-          fill = Significance,
-          title = "Life expectancy at birth",
-          subtitle = "Males in Upper Tier Local Authorities England",
-          copyright_year = as.Date("2019-01-01"))
-
-ons_regions <- "https://opendata.arcgis.com/datasets/f99b145881724e15a04a8a113544dfc5_3.geojson"
 
 # Visual tests ------------------------------------------------------------
 
-test_that("map draws correctly despite difference date inputs", {
-        skip_if_offline()
+test_that("map renders correctly with year input to copyright_year", {
+        skip_on_cran()
+        df <- mapdata
+        ons_api <- "https://opendata.arcgis.com/datasets/687f346f5023410ba86615655ff33ca9_4.geojson"
+        p <- map(df,
+                 ons_api = ons_api,
+                 area_code = AreaCode,
+                 fill = Significance,
+                 title = "Life expectancy at birth",
+                 subtitle = "Males in Upper Tier Local Authorities England",
+                 copyright_year = 2019)
+
         vdiffr::expect_doppelganger("map", p)
+})
+test_that("map renders correctly with date input to copyright_year", {
+        skip_on_cran()
+        df <- mapdata
+        ons_api <- "https://opendata.arcgis.com/datasets/687f346f5023410ba86615655ff33ca9_4.geojson"
+        p1 <- map(df,
+                  ons_api = ons_api,
+                  area_code = AreaCode,
+                  fill = Significance,
+                  title = "Life expectancy at birth",
+                  subtitle = "Males in Upper Tier Local Authorities England",
+                  copyright_year = as.Date("2019-01-01"))
+
         vdiffr::expect_doppelganger("map p1", p1)
 })
 
-test_that("error messages work for map", {
+test_that("error where string passed to copyright_year in map fails", {
+        skip_on_cran()
+        df <- mapdata
+        ons_api <- "https://opendata.arcgis.com/datasets/687f346f5023410ba86615655ff33ca9_4.geojson"
         expect_error(map(df,
                          ons_api = ons_api,
                          area_code = AreaCode,
@@ -39,6 +44,12 @@ test_that("error messages work for map", {
                          subtitle = "Males in Upper Tier Local Authorities England",
                          copyright_year = "2018"),
                      "copyright_year must be either a 4 digit numeric class or Date class")
+})
+
+test_that("error where no obvious join field between shape file and data", {
+        skip_on_cran()
+        df <- mapdata
+        ons_regions <- "https://opendata.arcgis.com/datasets/f99b145881724e15a04a8a113544dfc5_3.geojson"
         expect_error(map(df,
                          ons_api = ons_regions,
                          area_code = AreaCode,
@@ -46,6 +57,9 @@ test_that("error messages work for map", {
                          title = "Life expectancy at birth",
                          subtitle = "Males in Upper Tier Local Authorities England"),
                      "There is no clear field in the shape file that contains the area codes in the field you have identified")
+})
+test_that("error where API is unavailable", {
+        df <- mapdata
         expect_error(map(df,
                          ons_api = "httpstat.us/500",
                          area_code = AreaCode,
@@ -53,6 +67,10 @@ test_that("error messages work for map", {
                          title = "Life expectancy at birth",
                          subtitle = "Males in Upper Tier Local Authorities England"),
                      "The ons_api provided is currently unavailable")
+})
+
+test_that("error where ons api url isn't passed to map function", {
+        df <- mapdata
         expect_error(map(df,
                          area_code = AreaCode,
                          fill = Significance,
